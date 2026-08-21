80d2dec6ac6ba78d117db3c08a0be97922135af8
Victor Hugo <victorhugo.prog@gmail.com>
2026-08-21 05:51:00 -0300

    Category

--- src/main/java/com/victorhugo/course/config/TestConfig.java
+++ src/main/java/com/victorhugo/course/config/TestConfig.java
@@ -1,8 +1,10 @@
 package com.victorhugo.course.config;
 
+import com.victorhugo.course.entities.Category;
 import com.victorhugo.course.entities.Order;
 import com.victorhugo.course.entities.User;
 import com.victorhugo.course.entities.enums.OrderStatus;
+import com.victorhugo.course.repositories.CategoryRepository;
 import com.victorhugo.course.repositories.OrderRepository;
 import com.victorhugo.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +25,17 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Electronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
